67f9f7089cc251fdecc3f976056c60d575afa6e7
LucasSilv01 <joselucascardoso5@gmail.com>
2026-05-15 17:28:11 -0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Three.js-Sketchfab/' : '',
+  base: process.env.NODE_ENV === 'production' ? './' : '',
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'three'
+            return 'vendor'
+          }
+        }
+      }
+    }
   }
 })
